9b20529148567bae7ae53a6f0d8f9be696f4a95c
Sean Langiewicz <slangiewicz.21@gmail.com>
2017-12-01 00:02:10 -0800

    Playing with player health bar

---
+++ 2D-Project/Assets/healthSystem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthSystem : MonoBehaviour
+{
+    public float maxHealth = 100;
+   public float currentHealth;
+  // public float maxHealth { get; set; }
+   //public float currentHealth { get; set; }
+    public Slider healthSlider;
+
+    private float calculatedHealth;
+
+    public float damage;
+
+
+
+    void Start()
+    {
+        //maxHealth = 20f;
+        currentHealth = maxHealth;
+        Debug.Log(currentHealth + "is the current health");
+       // healthSlider.value = CalculateHealth();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(5);
+       
+
+    }
+
+
+    void DealDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthSlider.value = CalculateHealth();
+        Debug.Log(damage + "Damage Done");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        currentHealth = 0;
+        Debug.Log("You are Dead");
+    }
+
+    float CalculateHealth()
+    {
+       
+      
+        calculatedHealth = (float)currentHealth / maxHealth;
+
+        Debug.Log("Current Health" + currentHealth);
+        Debug.Log("Max health" + maxHealth);
+        Debug.Log("Calc health" + calculatedHealth);
+
+        return calculatedHealth;
+        
+    }
+}
