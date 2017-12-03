5ba9d424e3fce24422f1c79d1f22047c7989c524
Sean Langiewicz <slangiewicz.21@gmail.com>
2017-12-02 23:37:48 -0800

    Added Energy regen

---
+++ 2D-Project/Assets/EnergySystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergySystem : MonoBehaviour
+{
+    [SerializeField] float maxEnergyPoints = 100f;
+    [SerializeField] float energyRegenPerSecond = 1.0f;
+    
+
+    public float SkillCost = 0f;
+    
+
+    public Slider energySlider;
+    public float energy;
+
+   private float currentEnergyPoints;
+
+
+    
+
+    void Start()
+    {
+        currentEnergyPoints = maxEnergyPoints;
+        energySlider.value = maxEnergyPoints;
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            
+            CalculateEnergy();
+           
+            
+
+        }
+
+        EnergyRegen();
+
+    }
+
+    private void EnergyRegen()
+    {
+        float pointsToAdd = energyRegenPerSecond * Time.deltaTime;
+        currentEnergyPoints = Mathf.Clamp(currentEnergyPoints + pointsToAdd, 0, maxEnergyPoints);
+        CalculateSlider();
+    }
+
+    private void CalculateEnergy()
+    {
+        currentEnergyPoints -= SkillCost;
+        CalculateSlider();
+    }
+
+    private void CalculateSlider()
+    {
+        energySlider.value = currentEnergyPoints;
+    }
+}
---
+++ 2D-Project/Assets/MyProject/Scripts/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed;
+
+    public Transform target;
+    public float chaseRange;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if(distanceToTarget < chaseRange)
+        {
+            //start chasing target - turn and move towards target
+            Vector3 targetDirection = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+
+    }
+      
+}
